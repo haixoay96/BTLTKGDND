e0bf0324224bc4c913ea4b4470104d0f5d71b5e1
Linh Đức <Linh c>
2016-10-11 14:29:31 +0700

    Screen login

---
+++ app/src/main/java/com/example/haixo/chatwithme/other/MyApplication.java
@@ -0,0 +1,15 @@
+package com.example.haixo.chatwithme.other;
+
+import android.app.Application;
+
+import io.socket.client.IO;
+import io.socket.client.Socket;
+
+/**
+ * Created by haixo on 10/11/2016.
+ */
+
+public class MyApplication extends Application {
+    public static String url;
+    public static Socket socket;
+}
