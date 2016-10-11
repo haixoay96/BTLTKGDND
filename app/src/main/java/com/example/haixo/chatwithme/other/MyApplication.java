8ea73b02ed59ea80a25482da272ecd155c6ea2e6
Linh Đức <Linh c>
2016-10-12 01:29:18 +0700

    add socket

--- app/src/main/java/com/example/haixo/chatwithme/other/MyApplication.java
+++ app/src/main/java/com/example/haixo/chatwithme/other/MyApplication.java
@@ -1,15 +1,43 @@
 package com.example.haixo.chatwithme.other;
 
 import android.app.Application;
+import android.util.Log;
+
+import java.net.URISyntaxException;
 
 import io.socket.client.IO;
 import io.socket.client.Socket;
+import io.socket.emitter.Emitter;
 
 /**
  * Created by haixo on 10/11/2016.
  */
 
 public class MyApplication extends Application {
-    public static String url;
+    public static String url = "http://192.168.0.107:3000";
     public static Socket socket;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        try {
+            socket = IO.socket("http://192.168.0.107:3000");
+            socket.on(Socket.EVENT_CONNECT, new Emitter.Listener() {
+                @Override
+                public void call(Object... args) {
+
+                }
+            });
+            socket.on(Socket.EVENT_DISCONNECT, new Emitter.Listener() {
+                @Override
+                public void call(Object... args) {
+
+                }
+            });
+            socket.connect();
+            Log.d("socket", "thanh cong");
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
 }
